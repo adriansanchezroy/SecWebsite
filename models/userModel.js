7da54a3917160ff385cad0e9f91bde9b05e210f1
Adrian Sanchez Roy <adriansanchezroy@gmail.com>
2023-03-23 19:34:56 -0400

    Modify userModel schema

--- models/userModel.js
+++ models/userModel.js
@@ -2,19 +2,12 @@ const { ref } = require("@hapi/joi/lib/compile");
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  name: {
+  username: {
     type: String,
     required: true,
     min: 6,
     max: 255,
   },
-  email: {
-    type: String,
-    required: true,
-    min: 6,
-    max: 255,
-  },
-
   password: {
     type: String,
     required: true,
